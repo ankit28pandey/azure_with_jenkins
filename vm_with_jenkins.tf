a58cb48c258313d19ef7bb29afa51d66ffa5ea9a
Ankit Pandey <91360605+ankit28pandey@users.noreply.github.com>
2022-11-13 12:38:26 +0530

    Update vm_with_jenkins.tf

--- vm_with_jenkins.tf
+++ vm_with_jenkins.tf
@@ -29,7 +29,7 @@ resource "azurerm_virtual_network" "example" {
 
 resource "azurerm_subnet" "example" {
     name = "terraform-subnet"
-    address_space = ["10.0.0.0/24"]
+    address_prefixes = ["10.0.0.0/24"]
     resource_group_name = azurerm_resource_group.example.name
     virtual_network_name = azurerm_virtual_network.example.name
 }
